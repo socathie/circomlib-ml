444c98dc1b84df5e6bad70af9e18c8e86d111fae
Cathie So <socathie@gmail.com>
2022-11-25 23:45:02 +0800

    encrypt/decrypt tests

---
+++ test/circuits/encryptDecrypt_test.circom
@@ -0,0 +1,43 @@
+pragma circom 2.0.3;
+
+include "../../circuits/crypto/encrypt.circom";
+include "../../circuits/crypto/ecdh.circom";
+
+// from zk-ml/linear-regression-demo
+
+template Test() {
+    signal input message;
+    signal input shared_key;
+    signal output out;
+    signal input private_key;
+    signal input public_key[2];
+
+    component ecdh = Ecdh();
+
+    ecdh.private_key <== private_key;
+    ecdh.public_key[0] <== public_key[0];
+    ecdh.public_key[1] <== public_key[1];
+
+    log(ecdh.shared_key);
+    log(shared_key);
+    log(private_key);
+    log(public_key[0]);
+    log(public_key[1]);
+
+    shared_key === ecdh.shared_key;
+
+    component enc = Encrypt();
+    component dec = Decrypt();
+
+    message ==> enc.plaintext;
+    shared_key ==> enc.shared_key;
+    shared_key ==> dec.shared_key;
+    enc.out[0] ==> dec.message[0];
+    enc.out[1] ==> dec.message[1];
+
+    log(dec.out);
+    dec.out === message;
+    out <== 1;
+}
+
+component main = Test();
--- test/encryption.js
+++ test/encryption.js
@@ -9,7 +9,9 @@ exports.p = Scalar.fromString("2188824287183927522224640574525727508854836440041
 const Fr = new F1Field(exports.p);
 
 const assert = chai.assert;
+
 const { Keypair } = require("./modules/maci-domainobjs");
+const { encrypt, decrypt } = require("./modules/maci-crypto");
 
 describe("crypto circuits test", function () {
     this.timeout(100000000);
@@ -31,4 +33,116 @@ describe("crypto circuits test", function () {
         assert(Fr.eq(Fr.e(witness[1]), Fr.e(keypair.pubKey.rawPubKey[0])));
         assert(Fr.eq(Fr.e(witness[2]), Fr.e(keypair.pubKey.rawPubKey[1])));
     });
+
+    it("ecdh shared key test", async () => {
+        const circuit = await wasm_tester(path.join(__dirname, "circuits", "ecdh_test.circom"));
+
+        const keypair = new Keypair();
+        const keypair2 = new Keypair();
+
+        const ecdhSharedKey = Keypair.genEcdhSharedKey(
+            keypair.privKey,
+            keypair2.pubKey,
+        );
+
+        const INPUT = {
+            'private_key': keypair.privKey.asCircuitInputs(),
+            'public_key': keypair2.pubKey.asCircuitInputs(),
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        //console.log(witness);
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(ecdhSharedKey)));
+
+    });
+
+    it("encrypt/decrypt test", async () => {
+        const circuit = await wasm_tester(path.join(__dirname, "circuits", "encryptDecrypt_test.circom"));
+
+        const keypair = new Keypair();
+        const keypair2 = new Keypair();
+
+        const ecdhSharedKey = Keypair.genEcdhSharedKey(
+            keypair.privKey,
+            keypair2.pubKey,
+        );
+
+        const INPUT = {
+            'message': '123456789',
+            'shared_key': ecdhSharedKey.toString(),
+            'private_key': keypair.privKey.asCircuitInputs(),
+            'public_key': keypair2.pubKey.asCircuitInputs(),
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        //console.log(witness);
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)));
+
+    });
+
+    it("encrypt in circom, decrypt in js", async () => {
+        const circuit = await wasm_tester(path.join(__dirname, "circuits", "encrypt_test.circom"));
+
+        const keypair = new Keypair();
+        const keypair2 = new Keypair();
+
+        const ecdhSharedKey = Keypair.genEcdhSharedKey(
+            keypair.privKey,
+            keypair2.pubKey,
+        );
+
+        const plaintext = 1234567890n;
+
+        const INPUT = {
+            'plaintext': plaintext.toString(),
+            'shared_key': ecdhSharedKey.toString(),
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        output1 = witness[1];
+        output2 = witness[2];
+
+        const ciphertext = {
+            iv: output1,
+            data: [output2],
+        }
+
+        assert(Fr.eq(Fr.e(decrypt(ciphertext, ecdhSharedKey)[0]), Fr.e(plaintext)));
+    });
+
+    it("encrypt in js, decrypt in circom", async () => {
+        const circuit = await wasm_tester(path.join(__dirname, "circuits", "decrypt_test.circom"));
+
+        const keypair = new Keypair();
+        const keypair2 = new Keypair();
+
+        const ecdhSharedKey = Keypair.genEcdhSharedKey(
+            keypair.privKey,
+            keypair2.pubKey,
+        );
+
+        const plaintext = 1234567890n;
+
+        const ciphertext = encrypt([plaintext], ecdhSharedKey);
+
+        const INPUT = {
+            'message': [ciphertext.iv.toString(), ciphertext.data[0].toString()],
+            'shared_key': ecdhSharedKey.toString(),
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(plaintext)));
+
+    });
+    
+    // TODO: encrypt multiple
+    // TODO: decrypt multiple
 });
